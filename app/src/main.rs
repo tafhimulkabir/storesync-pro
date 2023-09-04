a8486cd5ea27ca2b1842d121e857bd82dfe1e4ec
Tafhimul kabir <tafhimulkabir@protonmail.com>
2023-09-05 02:40:32 +0600

    Add Dependencie for Adding Background Blur

--- app/src/main.rs
+++ app/src/main.rs
@@ -3,6 +3,7 @@
 
 use tauri::Manager;
 use window_shadows::set_shadow;
+use window_vibrancy::{apply_blur, apply_vibrancy, NSVisualEffectMaterial};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -17,6 +18,13 @@ fn main() {
             let window = app.get_window("main").unwrap();
             #[cfg(any(windows, target_os = "macos"))]
             set_shadow(&window, true).unwrap();
+
+            #[cfg(target_os = "macos")]
+            apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None).expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+
+            #[cfg(target_os = "windows")]
+            apply_blur(&window, Some((18, 18, 18, 125))).expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+
             Ok(())
         })
         .run(tauri::generate_context!())
